45ea56141605ea64ebfbd271dbf51413c1f3924e
Cesaromancalvo <cesarocabalon@gmail.com>
2026-09-18 20:03:55 +0200

    Create migration.sql

---
+++ apps/api/prisma/migrations/20260918000000_add_user_mfa/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN "totpSecret" TEXT,
+ADD COLUMN "totpEnabled" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN "mfaRecoveryCodes" TEXT[] NOT NULL DEFAULT ARRAY[]::TEXT[];
